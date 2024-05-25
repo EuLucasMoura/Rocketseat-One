4d586ffa7f07efb090e0803e57ce664d6430cfd5
Lucas Moura <123328537+EuLucasMoura@users.noreply.github.com>
2024-05-25 14:51:06 -0300

    Requisição post

--- Java/Fundamentos_do_SpringBoot/primeiro-projeto-springboot/src/main/java/br/com/eulucasmoura/Controller/PrimeiraController.java
+++ Java/Fundamentos_do_SpringBoot/primeiro-projeto-springboot/src/main/java/br/com/eulucasmoura/Controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,12 @@ public class PrimeiraController {
         return "O parâmetro com query params 2 é " + allParams.entrySet();
     }
 
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "Mêtodo com body params " + usuario.username();
+    }
 
+    record Usuario(String username) {
+
+    }
 }
